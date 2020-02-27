92177e2d362d65be6064badcac8ab67fb80c65af
Z-HNAN <zhn68033@163.com>
2020-02-28 01:01:34 +0800

    update package.json

--- dist/config.js
+++ dist/config.js
@@ -2,7 +2,6 @@
 const tslib_1 = require("tslib");
 const path = tslib_1.__importStar(require("path"));
 const config = {
-    VERSION: '0.1.0',
     DEFAULT_FOLDER: 'private',
     PATH: path.resolve('./'),
 };
--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,6 @@
 import * as path from 'path'
 
 const config: {
-  // 版本
-  VERSION: string
-
   // 默认文件夹名称
   DEFAULT_FOLDER: string
 
@@ -11,7 +8,6 @@ const config: {
   PATH: string
 
 } = {
-  VERSION: '0.1.0',
   DEFAULT_FOLDER: 'private',
   PATH: path.resolve('./'),
 }
